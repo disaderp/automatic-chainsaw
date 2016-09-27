8d840d0ec15a4f579c6be702c6990fb37941fb2d
Vanderpool <vanderpool0312@gmail.com>
2016-09-27 19:41:19 +0200

    Added appropiate modules

--- GPU/GPU.v
+++ GPU/GPU.v
@@ -2,11 +2,49 @@ module GPU(
 input clk,
 input reset,
 input [15:0] cpuline
+input clr,
+input dat,
+input pixh,
+input pixv,
+input adress,
+output out_vga,
+output dis_mem_en,
+output font_mem_en,
+output asciiadress,
+output hsync,
+output vsync,
+output dis_en,
+output out
 //@todo: wypisac wszystkie inputy i outputy z nizszych modulow)
 );
 
 //zainicjowac TXT podpiac sygnaly, zainscjonac FOnt rom
-
+TXT d0 (
+		.clk (clk),
+		.clr (clr),
+		.pixh (pixh),
+		.pixv (pixv),
+		.dat (dat),
+		.out_vga (out_vga),
+		.asciiadress (asciiadress),
+		.font_mem_en (font_mem_en),
+		.dis_mem_en (dis_mem_en)
+		);
+VGA z0 (
+		.clk (clk),
+		.clr (clr),
+		.dat (dat),
+		.vsync (vsync),
+		.hsync (hsync),
+		.pixv (pixv),
+		.pixh (pixh),
+		.dis_en (dis_en)
+		);
+font_rom x0 (
+		.adress (adress),
+		.out (out)
+		);
+		
 reg [15:0] cmd;
 reg [15:0] param;
 
@@ -18,6 +56,23 @@ reg [11:0] tmpy;//line //=0
 reg nopsate;//inicjalizacja 0
 reg [15:0] nextcmd;
 //@todo: blok reset always//patrz cpu.v//zainicjum cmd=0;
+always @ (posedge clk) begin: clr
+	if (clr) disable clr
+	cpuline <= 0;
+	dat <= 0;
+	pixh <= 0;
+	pixv <= 0;
+	out_vga <= 0;
+	dis_mem_en <= 0;
+	font_mem_en <= 0;
+	hsync <= 0;
+	vsync <=0;
+	asciiadress <=0;
+	dis_en <= 0;
+	adress <=0;
+	out <=0;
+	end
+	
 always @(posedge clk) begin : main
 //@todo: disaable
 	case (cmd) begin
