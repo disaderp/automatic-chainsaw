a4db220ad9e1411dfa466123fea6f5f762fa5a82
Disa <dis@mail.rotf.pl>
2016-09-29 16:18:01 +0200

    assembler fixes

--- ASM/CPUAssembler/Asm.vb
+++ ASM/CPUAssembler/Asm.vb
@@ -43,8 +43,6 @@ Module Asm
 				ic -= 1
 			ElseIf mn.StartsWith("X") Then
 				code += Trail16(mn.Substring(1))
-			ElseIf mn = "" Then
-				ic -= 1
 			ElseIf mn.StartsWith("NOP") Then
 				code += Trail16("0")
 			ElseIf mn.StartsWith("SFC") Then
@@ -52,10 +50,14 @@ Module Asm
 				ic += 1
 			ElseIf mn.StartsWith("CFF") Then
 				code += Trail16("10")
-			ElseIf mn.StartsWith("CFF") Then
+			ElseIf mn.StartsWith("COF") Then
 				code += Trail16("11")
-			ElseIf mn.StartsWith("CFF") Then
+			ElseIf mn.StartsWith("CZF") Then
 				code += Trail16("100")
+			ElseIf mn.StartsWith("CBP") Then
+				code += Trail16("101101")
+			ElseIf mn.StartsWith("CPC") Then
+				code += Trail16("101110")
 			ElseIf mn.StartsWith("MOV [") Then
 				code += Trail16("101")
 				Dim params As String() = mn.Remove(0, 5).Split(",")
@@ -261,40 +263,357 @@ Module Asm
 
 			ElseIf mn.StartsWith("ADD") Then
 				code += Trail16("1100")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("ADC") Then
 				code += Trail16("1101")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("SUB") Then
 				code += Trail16("1110")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("SUC") Then
 				code += Trail16("1111")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("MUL8") Then
 				code += Trail16("10000")
+				Dim params As String() = mn.Remove(0, 5).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("MUL6") Then
 				code += Trail16("10001")
+				Dim params As String() = mn.Remove(0, 5).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("DIV8") Then
 				code += Trail16("10010")
+				Dim params As String() = mn.Remove(0, 5).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("DIV6") Then
 				code += Trail16("10011")
+				Dim params As String() = mn.Remove(0, 5).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("CMP") Then
 				code += Trail16("10100")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
+
 
 			ElseIf mn.StartsWith("AND") Then
 				code += Trail16("10101")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("NEG") Then
 				code += Trail16("10110")
+				Dim params As String = mn.Remove(0, 4)
+				Dim regs As String
+				If params = "AX" Then
+					regs = "00"
+				ElseIf params = "BX" Then
+					regs = "01"
+				ElseIf params = "CX" Then
+					regs = "10"
+				ElseIf params = "DX" Then
+					regs = "11"
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("NOT") Then
 				code += Trail16("10111")
+				Dim params As String = mn.Remove(0, 4)
+				Dim regs As String
+				If params = "AX" Then
+					regs = "00"
+				ElseIf params = "BX" Then
+					regs = "01"
+				ElseIf params = "CX" Then
+					regs = "10"
+				ElseIf params = "DX" Then
+					regs = "11"
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("OR") Then
 				code += Trail16("11000")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("SHL") Then
 				code += Trail16("11001")
+				Dim params As String = mn.Remove(0, 4)
+				Dim regs As String
+				If params = "AX" Then
+					regs = "00"
+				ElseIf params = "BX" Then
+					regs = "01"
+				ElseIf params = "CX" Then
+					regs = "10"
+				ElseIf params = "DX" Then
+					regs = "11"
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("SHR") Then
 				code += Trail16("11010")
+				Dim params As String = mn.Remove(0, 4)
+				Dim regs As String
+				If params = "AX" Then
+					regs = "00"
+				ElseIf params = "BX" Then
+					regs = "01"
+				ElseIf params = "CX" Then
+					regs = "10"
+				ElseIf params = "DX" Then
+					regs = "11"
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("XOR") Then
 				code += Trail16("11011")
+				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim regs As String
+				If params(0) = "AX" Then
+					regs = "00"
+				ElseIf params(0) = "BX" Then
+					regs = "01"
+				ElseIf params(0) = "CX" Then
+					regs = "10"
+				ElseIf params(0) = "DX" Then
+					regs = "11"
+				End If
+				If params(1) = "AX" Then
+					regs = Trail16(regs + "00")
+				ElseIf params(1) = "BX" Then
+					regs = Trail16(regs + "01")
+				ElseIf params(1) = "CX" Then
+					regs = Trail16(regs + "10")
+				ElseIf params(1) = "DX" Then
+					regs = Trail16(regs + "11")
+				End If
+				code += regs
+				ic += 1
 			ElseIf mn.StartsWith("TEST") Then
 				code += Trail16("11100")
-				Dim params As String() = mn.Remove(0, 4).Split(",")
+				Dim params As String() = mn.Remove(0, 5).Split(",")
 				Dim regs As String
 				If params(0) = "AX" Then
 					regs = "00"
