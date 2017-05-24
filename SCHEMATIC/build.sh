274734b4f1850d61e4f3a870863668f103211a35
Disa <dis@mail.rotf.pl>
2017-05-24 23:13:39 +0200

    eee

--- SCHEMATIC/build.sh
+++ SCHEMATIC/build.sh
@@ -8,7 +8,8 @@ elif [ "${BUILD}" = "COM" ]; then
 elif [ "${BUILD}" = "CPU" ]; then
 	./verilog-10.1.1/iverilog-vpi -Wall -g2012 -s CPU -o compiled.vvp ./CPU/ALU.v   ./CPU/RAM.v ./CPU/CPU.v ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
 elif [ "${BUILD}" = "GPU" ]; then
-	./verilog-10.1.1/ivl -Wall -g2012 -s GPU -o compiled.vvp ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
+	./verilog-10.1.1/iverilog-vpi -Wall -g2012 -s GPU -o compiled.vvp ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
+	./verilog-10.1.1/iverilog-vpi -h
 elif [ "${BUILD}" = "OS" ]; then
 	node ./COMPILER/compile.js ./OS/main.c
 fi
