5b0c962400dcb66de1d33e50ab5273596857221e
Disa <dis@mail.rotf.pl>
2017-05-24 20:28:58 +0200

    TRAVISWHYUDODIS

--- SCHEMATIC/build.sh
+++ SCHEMATIC/build.sh
@@ -2,13 +2,13 @@
 set -ev
 
 if [ "${BUILD}" = "ASM" ]; then
-	xbuild /p:Configuration=Release ../ASM/CPUAssembler/CPUAssembler.sln
+	msbuild /p:Configuration=Release ./ASM/CPUAssembler/CPUAssembler.sln
 elif [ "${BUILD}" = "COM" ]; then
-	node -c compile.js
+	node ./COMPILER/compile.js ./COMPILER/sample.cmm
 elif [ "${BUILD}" = "CPU" ]; then
-	verilog -Wall -g2012 -s CPU -o tmp/compiled.vvp ../CPU/ALU.v   ../CPU/RAM.v ../CPU/CPU.v ../GPU/Font_ROM.v ../GPU/TXT.v ../GPU/VGA.v ../GPU/GPU.v
+	iverilog -Wall -g2012 -s CPU -o compiled.vvp ./CPU/ALU.v   ./CPU/RAM.v ./CPU/CPU.v ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
 elif [ "${BUILD}" = "GPU" ]; then
-	verilog -Wall -g2012 -s GPU -o tmp/compiled.vvp ../GPU/Font_ROM.v ../GPU/TXT.v ../GPU/VGA.v ../GPU/GPU.v
+	iverilog -Wall -g2012 -s GPU -o compiled.vvp ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
 elif [ "${BUILD}" = "OS" ]; then
-	node compile.js ../OS/main.c
+	node ./COMPILER/compile.js ./OS/main.c
 fi
