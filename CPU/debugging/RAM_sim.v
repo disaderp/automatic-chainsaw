a57502d1c694f2ef341dae56a5c7e8ff82ef2087
Disa <dis@mail.rotf.pl>
2017-05-25 00:18:34 +0200

    ohyes

---
+++ CPU/debugging/RAM_sim.v
@@ -0,0 +1,23 @@
+module RAM(
+	input [11:0] ADDR1,
+    input [11:0] ADDR2,
+    input [11:0] ADDR3,
+    input [11:0] ADDR4,
+    input [11:0] ADDW1,
+    input [15:0] DATA,
+    input WREN,
+    output [15:0] DATA1,
+    output [15:0] DATA2,
+    output [15:0] DATA3,
+    output [15:0] DATA4,
+    input clk);
+    
+    reg [15:0] ram [1000:0];
+	always @(posedge clk) begin
+		DATA1 = ram[ADDR1];
+		DATA2 = ram[ADDR2];
+		DATA3 = ram[ADDR3];
+		DATA4 = ram[ADDR4];
+		if (WREN) ram[ADDW1] = DATA;
+	end
+endmodule
--- SCHEMATIC/build.sh
+++ SCHEMATIC/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -ev
 
-if [ "${BUILD}" = "ASM" ]; then
-	xbuild /p:Configuration=Release ./ASM/CPUAssembler/CPUAssembler.sln
-elif [ "${BUILD}" = "COM" ]; then
+if [ "${BUILD}" = "COM" ]; then
 	node ./COMPILER/compile.js ./COMPILER/define.cmm
 elif [ "${BUILD}" = "CPU" ]; then
-	iverilog -Wall -g2012 -s CPU -o compiled.vvp ./CPU/ALU.v  ./CPU/RAM.v ./CPU/CPU.v ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
+	iverilog -Wall -g2012 -s CPU -o compiled.vvp ./CPU/ALU.v ./CPU/debugging/RAM_sim.v ./CPU/CPU.v ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
 elif [ "${BUILD}" = "GPU" ]; then
 	iverilog -Wall -g2012 -s GPU -o compiled.vvp ./GPU/Font_ROM.v ./GPU/TXT.v ./GPU/VGA.v ./GPU/GPU.v
 elif [ "${BUILD}" = "OS" ]; then
