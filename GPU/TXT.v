df8a12598f373e4977bd526668ffb70f0e239eb1
Vanderpool <vanderpool0312@gmail.com>
2017-05-31 00:27:20 +0200

    Only here! Read about 5 tricks to make your code professional in just few seconds!

--- GPU/TXT.v
+++ GPU/TXT.v
@@ -1,71 +1,3 @@
-/*module TXT(
-input clk,		//clock
-input clr,		//clear
-input [7:0] char_line_dat,	//data
-output reg out_vga, //vga output
-output reg [11:0] asciiaddress, 
-output reg dis_mem_en,		//enables display memory
-output reg font_mem_en,		//enables font memory
-output [9:0] pix_x,
-output [9:0] pix_y,
-output hsync, output vsync
-);
-
-reg [7:0] line;		//sth
-reg [11:0] hp;		//sth else
-reg [11:0] vp;		//sth else2
-//wire [9:0] pixv;	//pixel vertical location
-//wire [9:0] pixh;
-VGA vga0 (
-		.clk (clk),
-		//.clr (clr),
-		.pixv (pix_x),
-		.pixh (pix_y),
-		.hsync(hsync),
-		.vsync(vsync)
-		);
-//assign pix_x = pixh;
-//assign pix_y = pixv;
-
-always @ (posedge clk)//or negedge clr
-begin
-
-		asciiaddress <= 0;
-			case (pix_x[2:0])
-				3'b110: begin
-					hp[11:0] <= { 6'd0, pix_x[9:4] };
-					vp[11:0] <= { 6'd0, pix_y[9:4] };
-					asciiaddress[11:0] <= hp + (vp << 5) + (vp << 3); //40 = 2*2*2*2*2 + 2*2*2
-					dis_mem_en <= 1;
-					font_mem_en <= 1;
-				end
-	
-				3'b111: begin
-					line <= char_line_dat;
-				end
-				3'b000: begin
-					dis_mem_en <= 0;
-					font_mem_en <=0;
-				end
-			endcase
-end
-
-always @ (posedge clk)
-begin
-	case (pix_x[2:0])
-	3'b000: out_vga <= line[7];
-	3'b001: out_vga <= line[6];
-	3'b010: out_vga <= line[5];
-	3'b011: out_vga <= line[4];
-	3'b100: out_vga <= line[3];
-	3'b101: out_vga <= line[2];
-	3'b110: out_vga <= line[1];
-	3'b111: out_vga <= line[0];
-	endcase
-end
-
-endmodule
-*/
 // Module SEQUENCER
 //https://baltazarstudios.com/poem-fpga/
 // Sequences the ASCII character data from display ROM, through the font ROM, and out
@@ -98,7 +30,7 @@ VGA vga0 (
 		.vsync(vsync)
 		);
  
-//======================================================================
+
  
 reg [7:0] line_data;             // Current character line data shifter
 reg [11:0] xp;                   // Temp
--- GPU/VGA.v
+++ GPU/VGA.v
@@ -1,51 +1,4 @@
-/*module VGA
-(
-	input clk,	//clock
-	input clr,	//clear
-	output reg[9:0] pixv,	//pixel vertical location
-	output reg[9:0] pixh,	//pixel horizontal location
-	output hsync,
-	output vsync
-	
-);
-	parameter hpix=1056;    //horizontal pixel count
-	parameter hsp=128;      //horizontal sync pulse
-	parameter hbp=216;      //horizontal back porch
-	parameter hfp=1016;     //horizontal front porch
-	parameter vpix=628;     //vertical pixel count
-	parameter vsp=4;        //vertical sync pulse
-	parameter vbp=23;       //vertical back porch
-	parameter vfp=627;      //vertical front porch
-	
-reg [9:0] hcounter = 0;     //horizontal counter
-reg [9:0] vcounter = 0;     //vertical counter
 
-//only triggered on signal transitions or edges
-//posedge = falling edge = rising edge and negedge
-always @(posedge clk) begin
-	//counts until end of the line 
-	if (hcounter < hpix - 1) hcounter <= hcounter +1;
-	else begin
-		//end of the line = horizontal counter reset + vertical counter increase
-		//if vertical counter is at the end = reset of both counters
-		hcounter <= 0;
-		if (vcounter < vpix - 1) vcounter <= vcounter +1;
-		else vcounter <=0;
-	end
-
-	if (vcounter >= vbp+vsp && vcounter < vfp) begin
-		if (hcounter >= 256 && hcounter < hpix) begin
-			pixh <= hcounter - 10'd256;
-			pixv <= vcounter - 10'd27;
-		end
-	end
-end
-
-//synchronization pulses generation
-assign hsync = (hcounter < hsp) ? 0:1;    //horizontal sync pulse gen
-assign vsync = (vcounter < vsp) ? 0:1;    //vertical sync pulse gen
-
-endmodule*/
 // Module VGA_SYNC
 //
 // Generates output sync signals to drive VGA display in 640x480 pixel mode:
@@ -72,7 +25,7 @@ output reg vsync,            // Output vertical sync signal
 output reg[9:0] pixh,           //  x-coordinate of an active pixel
 output reg[9:0] pixv            //  y-coordinate of an active pixel
 );
-//======================================================================
+
 
 localparam SYNC_ON  = 1'b0;      // Define the polarity of sync pulses
 localparam SYNC_OFF = 1'b1;
