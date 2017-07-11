249a1554a86e538f0b8e11ebf8d8c660ca8354eb
theseafaringturtle <theseafaringturtle@gmail.com>
2017-07-11 22:36:46 +0200

    Update OPER.h

--- OS/OPER.h
+++ OS/OPER.h
@@ -1,7 +1,8 @@
 bool char_compare (char *x, char *y){
 	while (*x != 0 || *y != 0){
-		if (*x != *y)
+		if (*x != *y){
 			return false;
+		}
 		x++;
 		y++;
 	}
@@ -9,12 +10,14 @@ bool char_compare (char *x, char *y){
 }
 
 bool char_compare_l (char *x, char *y, int l){
-	if (l < 0) 
+	if (l < 0) {
 		return false;
+	}
 	int i = 0;
 	while (i != l){
-		if (*x != *y)
+		if (*x != *y){
 			return false;
+		}
 		x++;
 		y++;
 		i++;
