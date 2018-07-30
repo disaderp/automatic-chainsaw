6010713077fd5c0a57063ee45e14c000d2790eba
Disa <dis@mail.rotf.pl>
2018-07-30 21:32:54 +0200

    do it pls

--- SCHEMATIC/installv.sh
+++ SCHEMATIC/installv.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ev
 
-sudo apt-get update
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+sudo apt install apt-transport-https
+echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt-get -y install mono-complete
 sudo apt-get -y install mono-runtime
 sudo apt-get -y install libmono-system-core4.0-cil
 sudo apt-get -y install libmono-corlib4.5-cil
