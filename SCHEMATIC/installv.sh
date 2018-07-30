ab95b4416fdb9fd6c176b40960fbb24642fc0bb1
Disa <dis@mail.rotf.pl>
2018-07-30 21:36:24 +0200

    nowornever

--- SCHEMATIC/installv.sh
+++ SCHEMATIC/installv.sh
@@ -7,10 +7,10 @@ echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | su
 sudo apt update
 sudo apt-get -y install mono-complete
 sudo apt-get -y install mono-runtime
-sudo apt-get -y install libmono-system-core4.0-cil
-sudo apt-get -y install libmono-corlib4.5-cil
-sudo apt-get -y install mono-basic
-sudo apt-get -y install libmono-microsoft-visualbasic10.0-cil
+#sudo apt-get -y install libmono-system-core4.0-cil
+#sudo apt-get -y install libmono-corlib4.5-cil
+#sudo apt-get -y install mono-basic
+#sudo apt-get -y install libmono-microsoft-visualbasic10.0-cil
 
 wget ftp://icarus.com/pub/eda/verilog/v10/verilog-10.1.1.tar.gz -O v.tar.gz
 tar -xzf v.tar.gz
