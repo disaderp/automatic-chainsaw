2691216dbbde1789890dd9d7914482f4774b419c
Disa <dis@mail.rotf.pl>
2018-07-30 21:21:41 +0200

    moar

--- SCHEMATIC/installv.sh
+++ SCHEMATIC/installv.sh
@@ -4,7 +4,8 @@ set -ev
 sudo apt-get -y install mono-runtime
 sudo apt-get -y install libmono-system-core4.0-cil
 sudo apt-get -y install libmono-corlib4.5-cil
-sudo apt-get -y install libmono-microsoft-visualbasic10.0-cil
+sudo apt-get -y install libmono-microsoft-visualbasic8.0-cil
+sudo apt-get -y install libmono-microsoft-visualbasic11.0-cil
 
 wget ftp://icarus.com/pub/eda/verilog/v10/verilog-10.1.1.tar.gz -O v.tar.gz
 tar -xzf v.tar.gz
